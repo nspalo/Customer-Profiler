d83608498b5f1dcf68b6a7e926745f711f3d7fff
Noel Palo <noelspalo@gmail.com>
2021-02-03 20:45:30 +0800

    Add customer renderer class

---
+++ app/Database/Renderers/Customer/CustomerRenderer.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Database\Renderers\Customer;
+
+use App\Database\Entities\Customer\Customer;
+use App\Database\Renderers\Render;
+
+/**
+ * Class CustomerRenderer
+ * @package App\Database\Renders\Customer
+ */
+class CustomerRenderer extends Render
+{
+    /**
+     * @param Customer $customer
+     * @return array
+     */
+    public function render(Customer $customer): array
+    {
+        return [
+            'id' => $customer->getId(),
+            'emailAddress' => $customer->getEmailAddress(),
+            'fullName' => $customer->getFirstName() . " " . $customer->getLastName(),
+            'firstName' => $customer->getFirstName(),
+            'lastName' => $customer->getLastName(),
+            'username' => $customer->getUsername(),
+            'gender' => $customer->getGender(),
+            'phone' => $customer->getPhone(),
+            'city' => $customer->getCity(),
+            'country' => $customer->getCountry()
+        ];
+    }
+
+}
