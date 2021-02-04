1a6530c3efa8f4712289055026e4bedc72afc777
Noel Palo <noelspalo@gmail.com>
2021-02-04 11:16:56 +0800

    Add the initial import service class

--- app/Console/Commands/DataImport/ImportCustomersCommand.php
+++ app/Console/Commands/DataImport/ImportCustomersCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands\DataImport;
 
+use App\Services\Customers\CollectCustomerDataService;
 use Illuminate\Console\Command;
 
 /**
@@ -23,15 +24,20 @@ class ImportCustomersCommand extends Command
      * @var string
      */
     protected $description = 'Fetch and store a minimum of 100 users from a 3rd party data provider api.';
+    /**
+     * @var CollectCustomerDataService
+     */
+    private $collectCustomerDataService;
 
     /**
      * Create a new command instance.
      *
-     * @return void
+     * @param CollectCustomerDataService $collectCustomerDataService
      */
-    public function __construct()
+    public function __construct(CollectCustomerDataService $collectCustomerDataService)
     {
         parent::__construct();
+        $this->collectCustomerDataService = $collectCustomerDataService;
     }
 
     /**
@@ -41,21 +47,27 @@ class ImportCustomersCommand extends Command
      */
     public function handle()
     {
+        echo "fetching data..." . PHP_EOL;
 
+        $response = $this->fetchCustomerData();
+        $this->collectCustomerDataService->handle($response["results"]);
+
+        echo "Total number of data fetch: " . count($response["results"]);
+    }
+
+    /**
+     * @return mixed
+     */
+    private function fetchCustomerData()
+    {
         $filterHeadCount = 100;
         $filterNationality = "au";
         $filterFields   = "email,name,gender,phone,login,location,nat";
         $apiQueryString = "?results={$filterHeadCount}&nat={$filterNationality}&inc={$filterFields}&noinfo";
         $apiEndpoint    = "https://randomuser.me/api".$apiQueryString;
 
-        echo "API Endpoint: " . $apiEndpoint . PHP_EOL;
-        echo "fetching data..." . PHP_EOL;
-
         $response = file_get_contents($apiEndpoint);
-        $response = json_decode($response, true);
 
-        var_dump($response);
-
-        echo "Total number of data fetch: " . count($response["results"]);
+        return json_decode($response, true);
     }
 }
